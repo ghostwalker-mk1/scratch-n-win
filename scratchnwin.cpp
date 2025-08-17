ee270eabd66f15f38cdf128879739e5040efe8e5
ghostwalker-mk1 <114323343+ghostwalker-mk1@users.noreply.github.com>
2025-08-17 18:08:21 +0100

    add initial framework

---
+++ scratchnwin.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>
+
+class Player {
+    public:
+        int money;
+};
+
+class Ticket {
+    public:
+        int value1;
+        int value2;
+        int value3;
+};
+

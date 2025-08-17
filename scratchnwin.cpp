70b31b68921cf5cec00b4624672424c34ffe8a1a
ghostwalker-mk1 <114323343+ghostwalker-mk1@users.noreply.github.com>
2025-08-17 19:22:07 +0100

    create array of ticket states

--- scratchnwin.cpp
+++ scratchnwin.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h> 
 #include <time.h>
 #include <iostream>
+#include <array>
 
 using namespace std;
 
@@ -25,13 +26,35 @@ int main () {
     ticket.v2 = rand() % 9 + 1;
     ticket.v3  = rand() % 9 + 1;
 
-    
+    array<string, 4> banners = {
+    " --SCRATCH 'N' WIN-- \n"
+    "|    ___ ___ ___    |\n"
+    "|   |   |   |   |   |\n"
+    "|    --- --- ---    |\n"
+    " ------------------- \n",
 
-    printf(" --SCRATCH 'N' WIN-- \n");
-    printf("|    ___ ___ ___    |\n");
-    printf("|   | %i | %i | %i |   |\n", ticket.v1, ticket.v2, ticket.v3);
-    printf("|    --- --- ---    |\n");
-    printf(" ------------------- \n");
+    " --SCRATCH 'N' WIN-- \n"
+    "|    ___ ___ ___    |\n"
+    "|   | " + to_string(ticket.v1) + " |   |   |   |\n"
+    "|    --- --- ---    |\n"
+    " ------------------- \n",
+
+    " --SCRATCH 'N' WIN-- \n"
+    "|    ___ ___ ___    |\n"
+    "|   | " + to_string(ticket.v1) + " | " + to_string(ticket.v2) + " |   |   |\n"
+    "|    --- --- ---    |\n"
+    " ------------------- \n",
+
+    " --SCRATCH 'N' WIN-- \n"
+    "|    ___ ___ ___    |\n"
+    "|   | " + to_string(ticket.v1) + " | " + to_string(ticket.v2) + " | " + to_string(ticket.v3) + " |   |\n"
+    "|    --- --- ---    |\n"
+    " ------------------- \n",
+    };
+
+    for (const auto& banner : banners) {
+        cout << banner << "\n\n";
+    }
 
     return 0;
 

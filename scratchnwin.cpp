19ec4887d739fc94a3f560ce1cb0361448501d9e
ghostwalker-mk1 <114323343+ghostwalker-mk1@users.noreply.github.com>
2025-08-18 21:08:07 +0100

    add outcome/probability table

--- scratchnwin.cpp
+++ scratchnwin.cpp
@@ -3,29 +3,62 @@
 #include <time.h>
 #include <iostream>
 #include <array>
+#include <map>
 
 using namespace std;
 
-class Player {
-    public:
-        int money;
-};
-
-class Ticket {
-    public:
-        int v1, v2, v3;
-};
-
 int main () {
-    srand (time(NULL));
 
-    Player player;
-    Ticket ticket;
+    struct {
+        double p = 0.695;
+        int payout = 0;
+        string symbol = "💣";
+        double lowR = 0;
+        double highR = 0.695;
+    } lose;
 
-    ticket.v1 = rand() % 9 + 1;
-    ticket.v2 = rand() % 9 + 1;
-    ticket.v3  = rand() % 9 + 1;
+    struct {
+        double p = 0.170;
+        int payout = 1;
+        string symbol = "🍋";
+        double lowR = 0.695;
+        double highR = 0.865;
+    } small1;
 
+    struct {
+        double p = 0.070;
+        int payout = 2;
+        string symbol = "🍒";
+        double lowR = 0.865;
+        double highR = 0.935;
+    } small2;
+
+    struct {
+        double p = 0.040;
+        int payout = 5;
+        string symbol = "🍇";
+        double lowR = 0.935;
+        double highR = 0.975;
+    } mid;
+
+    struct {
+        double p = 0.015;
+        int payout = 10;
+        string symbol = "🔔";
+        double lowR = 0.975;
+        double highR = 0.990;
+    } big;
+
+    struct {
+        double p = 0.010;
+        int payout = 25;
+        string symbol = "⭐";
+        double lowR = 0.990;
+        double highR = 1.000;
+    } jackpot;
+
+
+    
     array<string, 4> banners = {
     " --SCRATCH 'N' WIN-- \n"
     "|    ___ ___ ___    |\n"
@@ -35,26 +68,22 @@ int main () {
 
     " --SCRATCH 'N' WIN-- \n"
     "|    ___ ___ ___    |\n"
-    "|   | " + to_string(ticket.v1) + " |   |   |   |\n"
+    "|   | " + to_string(ticket.value1) + " |   |   |   |\n"
     "|    --- --- ---    |\n"
     " ------------------- \n",
 
     " --SCRATCH 'N' WIN-- \n"
     "|    ___ ___ ___    |\n"
-    "|   | " + to_string(ticket.v1) + " | " + to_string(ticket.v2) + " |   |   |\n"
+    "|   | " + to_string(ticket.value1) + " | " + to_string(ticket.value2) + " |   |   |\n"
     "|    --- --- ---    |\n"
     " ------------------- \n",
 
     " --SCRATCH 'N' WIN-- \n"
     "|    ___ ___ ___    |\n"
-    "|   | " + to_string(ticket.v1) + " | " + to_string(ticket.v2) + " | " + to_string(ticket.v3) + " |   |\n"
+    "|   | " + to_string(ticket.value1) + " | " + to_string(ticket.value2) + " | " + to_string(ticket.value3) + " |   |\n"
     "|    --- --- ---    |\n"
     " ------------------- \n",
     };
-
-    for (const auto& banner : banners) {
-        cout << banner << "\n\n";
-    }
 
     return 0;
 
